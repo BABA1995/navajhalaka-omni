121298d4166fbf9f4f0ffec12ce1ef27c9e3e5de
diwakara <diwakara@navajhalaka.com>
2018-11-03 20:01:54 +0530

    app with auto start

--- app/src/main/java/com/app/navajhalaka/receiver/BootUpReceiver.java
+++ app/src/main/java/com/app/navajhalaka/receiver/BootUpReceiver.java
@@ -3,22 +3,16 @@ package com.app.navajhalaka.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
-import com.app.navajhalaka.app.BioscopeApp;
-
+import com.app.navajhalaka.ui.MainActivity;
 
 public class BootUpReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
-//            Intent i = new Intent(context, BioscopeApp.class);
-//            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//            Toast.makeText(context, "Boot Completed", Toast.LENGTH_SHORT).show();
-//            context.startActivity(i);
-//        for (int i = 0; i < 10; i++) {
-//            Toast.makeText(context, "boot completed", Toast.LENGTH_SHORT).show();
-//        }
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
     }
-
 
 }
